baca63313c892ddd13be151b766fcb98728744a2
Tiltusha <elcoronabooking@gmail.com>
2024-03-21 23:25:12 +0300

    header

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const swiperTop = new Swiper('.top__swiper', {
+    // Optional parameters
+    effect: 'fade',
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
